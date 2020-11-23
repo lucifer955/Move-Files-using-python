5c6f7c74c10264b71d63b0f3ee0ed28b72464b43
lucifer955 <ndr.nhk@gmail.com>
2020-11-23 23:19:36 +0530

    user input to get the file type folder name will be created according to the file type

--- FileOrganizer.py
+++ FileOrganizer.py
@@ -6,11 +6,14 @@ import shutil
 #get the directory path  where the files resides 
 src_path ='/home/lucifer955/Downloads/'
 
+#get user input
+file_type = input("Enter the file type here : ")
+
 #select the file type  
-arr = glob.glob(src_path + '*.pdf')
+arr = glob.glob(src_path + '*.%s' % file_type.lower())
 
 #name of the new directory
-directory  = "PDFs"
+directory  =  file_type.upper()+"s"
 
 #get the path the new directory should be created
 newPath = os.path.join(src_path,directory)
